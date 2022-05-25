c47d42bcade4a661dea45bceb25b8fbd109c42dd
Joao Dantas <joaovitordasilvadantas@outlook.com>
2022-05-24 21:21:33 -0300

    feat:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+import Tasks from "./components/Tasks";
+
+
+export default function App() {
+  const [tasks] = useState([
+    {
+      id: 1,
+      title: "Estudar Programação",
+      completed: true,
+    },
+    {
+      id: 2,
+      title: "Ler",
+      completed: false,
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <Tasks tasks={tasks}/>
+      </div>;
+    </>
   );
 }
-
-export default App;
---
+++ src/components/Task.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Task = ({task}) => {
+    return  <h1>{task.title}</h1> ;
+}
+ 
